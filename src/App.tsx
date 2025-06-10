2a07e1549c7eecbf489c49027910202291a57290
ryunosuke_arima <ryunosuke.arima125>
2025-06-10 21:47:33 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useEffect } from "react";
+import { useRef, useState } from "react";
+
+type Song = {
+  id: number;
+  title: string;
+  artist: string;
+  coverUrl: string;
+  musicUrl: string;
+};
+
+const songs: Song[] = [
+  {
+  id: 1,
+  title: "FIRST LOVE SONG",
+  artist: "1",
+  coverUrl: "public/ダウンロード (1).jpg",
+  musicUrl: "public/FIRST_LOVE_SONG.mp3",
+  },
+  {
+  id: 2,
+  title: "FLIGHT FEATHERS",
+  artist: "2",
+  coverUrl: "public/ダウンロード (2).jpg",
+  musicUrl: "public/FLIGHT_FEATHERS.mp3g",
+  },
+  {
+  id: 3,
+  title: "Howling over the World",
+  artist: "3",
+  coverUrl: "public/ダウンロード (3).jpg",
+  musicUrl: "public/Howling_over_the_World.mp3",
+  },
+  {
+  id: 4,
+  title: "Meteor Light",
+  artist: "4",
+  coverUrl: "public/ダウンロード (4).jpg",
+  musicUrl: "public/Meteor_Light.mp3",
+  },
+  {
+  id: 5,
+  title: "ORBITAL BEAT",
+  artist: "5",
+  coverUrl: "public/ダウンロード (5).jpg",
+  musicUrl: "public/ORBITAL_BEAT.mp3",
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const currentSong = songs[currentSongIndex];
+
+  const handlePrevious = () => {
+    setCurrentSongIndex(
+      (prevIndex) => (prevIndex - 1 + songs.length) % songs.length
+    );
+    setIsPlaying(false);
+  };
+
+  const handleNext = () => {
+    setCurrentSongIndex(
+      (prevIndex) => (prevIndex + 1) % songs.length
+    );
+    setIsPlaying(false);
+  }
+
+  const togglePlayPause = () => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  }
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={currentSong.coverUrl} alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div>
+        <h2>{currentSong.title}</h2>
+        <p>{currentSong.artist}</p>
+      </div>
+      <div>
+        <button onClick={handleNext}>進む</button>
+        <button onClick={togglePlayPause}>
+          {isPlaying ? "一時停止" : "再生"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={handlePrevious}>戻る</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <audio ref={audioRef} src={currentSong.musicUrl} />
+    </div>
   )
 }
-
-export default App
+export default App;
